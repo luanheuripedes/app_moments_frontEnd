3ebcc09ff112dce07041f329a55c40148a00583e
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-02-28 09:07:28 -0300

    adding Css in layout components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewMomentComponent } from './components/pages/new-moment/new-moment.component';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
   {path: 'about', component:AboutComponent},
+  {path: 'moments/new', component:NewMomentComponent},
 ];
 
 @NgModule({
