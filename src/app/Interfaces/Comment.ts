eae98ce1bc5e3adce154a9a2db6a525bf66dee72
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-02-28 21:38:44 -0300

    Created interfaces: moment, response and comment

---
+++ src/app/Interfaces/Comment.ts
@@ -0,0 +1,8 @@
+export interface Comment {
+  id: string;
+  text: string;
+  userName: string;
+  momentId: string;
+  created_at?: string;
+  updated_at?: string;
+}
--- src/app/Interfaces/Moment.ts
+++ src/app/Interfaces/Moment.ts
@@ -3,7 +3,7 @@ export interface Moment {
   title: string;
   description: string;
   imagem: string;
-  created_at: string;
-  updated_at: string;
+  created_at?: string;
+  updated_at?: string;
   comments: [{ text: string; username: string }];
 }
