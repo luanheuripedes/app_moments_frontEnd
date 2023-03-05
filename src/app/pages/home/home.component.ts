7176932b5c1c3f7541ce139bc940e6b01f2f19fa
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-03-05 17:07:55 -0300

    Resgatando os moments do banco e adicionado na pagina home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Interfaces/Moment';
+import { MessageService } from 'src/app/services/message.service';
+import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment.development';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  moments: Moment[] = [];
+  momentsfilter: Moment[] = [];
+  baseApiUril = environment.baseApiUrl;
 
+  // todo search
+  constructor(
+    private momentoService: MomentService,
+    private messageService: MessageService
+  ) {}
+  ngOnInit(): void {
+    this.momentoService.getAllMoments().subscribe({
+      next: (response) => {
+        const data = response.data;
+
+        data.map((item) => {
+          item.created_at = new Date(item.created_at!).toLocaleDateString(
+            'pt-BR'
+          );
+        });
+
+        this.moments = data;
+        this.momentsfilter = data;
+      },
+      error(error) {
+        console.log(error);
+      },
+    });
+  }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { Moment } from '../Interfaces/Moment';
+import { Response } from '../Interfaces/Response';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,9 @@ export class MomentService {
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
+  }
+
+  getAllMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
   }
 }
